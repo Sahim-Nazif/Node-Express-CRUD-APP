823a84a5cacd2fe157834e39c16a51cc6f17a752
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-13 16:23:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express()
+const dotenv=require('dotenv')
+const morgan=require('morgan');
+
+
+
+dotenv.config();
+
+if (process.env.NODE_ENV==='development') {
+    app.use(morgan('dev'))
+    console.log('the app is in development phase')
+
+} else if (process.env.NODE_ENV==='production') {
+
+    app.use(compress());
+    console.log('the app is in production phase ')
+}
+
+
+
+app.listen(process.env.PORT , ()=>{
+
+    console.log(`The app is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT }`)
+
+})
