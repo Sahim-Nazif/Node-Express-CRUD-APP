d5283dc465fabf9c1fd58df55fa60bbe6ffcc986
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-13 16:33:59 -0500

    added required middlewares

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express=require('express')
 const app=express()
 const dotenv=require('dotenv')
-const morgan=require('morgan');
+const morgan=require('morgan')
+const compress=require('compression')
+const bodyParser=require('body-parser')
 
 
 
 dotenv.config();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
+
+//static file access point
+app.use(express.static('public/css'));
+app.use(express.urlencoded({extended:true}))
+
 
 if (process.env.NODE_ENV==='development') {
     app.use(morgan('dev'))
