452f863381fd10531e045db99b0aca9beb7bf57a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-13 17:27:28 -0500

    added post routes for user registeration

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,15 +1,45 @@
-const User=require('../models/user')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
+const register_user = async (req, res) => {
 
-const register_user=(req, res)=>{
+    try {
+        const { firstName, lastName, email, password, confirmPassword } = req.body
+        const existingUser = await User.findOne({ email })
 
-    const{firstName, lastName, email, password, confirmPassword}=req.body
+        if (password !== confirmPassword) {
 
-    
-      
+            res.status(400).json({ message: 'Passwords do not match !' })
+        }
+        else if (!existingUser) {
+            const hashPassword = await bcrypt.hash(password, 10)
+            const user = await User.create({
+
+                firstName,
+                lastName,
+                email,
+                password: hashPassword
+            })
+            //will be returning all fields except for the password
+            return res.json({
+                _id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+            })
+        }
+
+        return res.status(400).json({ message: 'Email already exists! do you want to instead?' })
+
+    } catch (error) {
+
+        throw Error(`Error while registering a new user : ${error}`)
+    }
 }
 
-module.exports={
+
+
+module.exports = {
 
 
     register_user
