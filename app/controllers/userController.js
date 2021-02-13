eb12c026d2a20d6b9703e106390e5d656df4270c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-13 17:12:16 -0500

    Added user model and controller

---
+++ app/controllers/userController.js
@@ -0,0 +1,16 @@
+const User=require('../models/user')
+
+
+const register_user=(req, res)=>{
+
+    const{firstName, lastName, email, password, confirmPassword}=req.body
+
+    
+      
+}
+
+module.exports={
+
+
+    register_user
+}
