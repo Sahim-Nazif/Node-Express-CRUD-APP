550701c0716b485054cab98773f3c06ca90c1b22
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-13 18:47:49 -0500

    update server page with middlewares

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,17 +1,18 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
+//registering a user
 const register_user = async (req, res) => {
 
     try {
-        const { firstName, lastName, email, password, confirmPassword } = req.body
+      
+        const { firstName, lastName, email, password, confirmPassword } = req.body;
         const existingUser = await User.findOne({ email })
-
         if (password !== confirmPassword) {
-
-            res.status(400).json({ message: 'Passwords do not match !' })
+            res.status(200).json({message:'Passwords do not match !'});
         }
-        else if (!existingUser) {
+        
+        else if(!existingUser) {
             const hashPassword = await bcrypt.hash(password, 10)
             const user = await User.create({
 
@@ -22,23 +23,28 @@ const register_user = async (req, res) => {
             })
             //will be returning all fields except for the password
             return res.json({
-                _id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email
+                _id:user._id,
+                firstName:user.firstName,
+                lastName:user.lastName,
+                email:user.email
             })
         }
 
-        return res.status(400).json({ message: 'Email already exists! do you want to instead?' })
+        return res.status(400).json({ message: 'Email already exists! do you want to register instead?' })
 
     } catch (error) {
 
+       
         throw Error(`Error while registering a new user : ${error}`)
     }
 }
 
+//user login
+const user_login=(req, res)=>{
 
 
+    
+}
 module.exports = {
 
 
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express=require('express')
 const app=express()
+const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const morgan=require('morgan')
 const compress=require('compression')
 const bodyParser=require('body-parser')
+const userRoutes=require('./app/routes/Routes')
 
 
+
+
+
+
+
+//middlewares
 
 dotenv.config();
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('views', './app/views');
-app.set('view engine', 'ejs');
 
-//static file access point
-app.use(express.static('public/css'));
+
 app.use(express.urlencoded({extended:true}))
-
-
+app.use(express.json())
 if (process.env.NODE_ENV==='development') {
     app.use(morgan('dev'))
     console.log('the app is in development phase')
@@ -27,6 +30,20 @@ if (process.env.NODE_ENV==='development') {
     console.log('the app is in production phase ')
 }
 
+//db connection
+mongoose
+    .connect(process.env.MONGO_URI,{
+        useNewUrlParser:true, 
+        useUnifiedTopology:true,
+        useCreateIndex: true, 
+      
+        })
+        .then(()=> console.log('Mongo-DB Connected...'))
+        .catch(err => console.log(err));
+//static file access point
+app.use(express.static('public/css'));
+
+app.use('/user',userRoutes)
 
 
 app.listen(process.env.PORT , ()=>{
