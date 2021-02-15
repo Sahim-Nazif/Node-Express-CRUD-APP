ca573298f097e525d8baba38e0b8196dc4f3b43c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-02-15 10:01:54 -0500

    update view pages

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,43 +1,112 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt=require('jsonwebtoken')
 
+const register_user= (req, res)=>{
+    const {firstName, lastName, email, password, confirmPassword}=req.body;
+    let errors=[];
+    //check if passwords don't match
+    if(password !==confirmPassword){
+        errors.push({msg: 'Password do not match !'});
+    } 
+    if (password.length<6){
+        errors.push({msg: 'Password should be at least 6 characters!'});
+    }
+    //checking if any of the above conditions are true, we will re-render the sign up page
+    if (errors.length>0){
+        res.render('register', {
+            errors,
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword
+        });
+        }
+        //now if Valdiation passes
+        else{
+            User.findOne({email})
+                .then(user=>{
+                    if (user){
+                        //checking if user exists(means emails is registered)
+                        req.flash('error_msg', 'This email is already registered')
+                        //errors.push({msg:'Email is already registered !'})
+                        res.render('register', {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            confirmPassword
+                        });
+                    }
+                    else{
+                        const newUser= new User({
+                            firstName,
+                            lastName,
+                            email,
+                            password
+                        });
+                        //password hashing
+                        bcrypt.genSalt(10,(err, salt)=>{
+                            bcrypt.hash(newUser.password, salt,
+                                (err, hash)=>{
+                                    if (err) throw err;
+                                    newUser.password=hash;
+                                    //saving user to DB
+                                    newUser.save()
+                                        .then(user=>{
+                                            req.flash('success_msg','You are now registered & can login')
+                                            res.redirect('/')
+                                          
+                                        })
+                                        .catch(err=>{
+                                            console.log(err)
+                                        });
+                                })
+                        })
+                    }
+                })
+    }
+};
 //registering a user
-const register_user = async (req, res) => {
+// const register_user = async (req, res) => {
 
-    try {
+//     try {
       
-        const { firstName, lastName, email, password, confirmPassword } = req.body;
-        const existingUser = await User.findOne({ email })
-        if (password !== confirmPassword) {
-            res.status(200).json({message:'Passwords do not match !'});
-        }
-        
-        else if(!existingUser) {
-            const hashPassword = await bcrypt.hash(password, 10)
-            const user = await User.create({
+//         const { firstName, lastName, email, password, confirmPassword } = req.body;
+//         const existingUser = await User.findOne({ email })
+//         if (password !== confirmPassword) {
+//             //res.status(200).json({message:'Passwords do not match !'});
+//             req.flash('error_msg', 'Passwords do not match !')
+//         }
+   
+//         else if(!existingUser) {
+//             const hashPassword = await bcrypt.hash(password, 10)
+//             const user = await User.create({
 
-                firstName,
-                lastName,
-                email,
-                password: hashPassword
-            })
-            //will be returning all fields except for the password
-            return res.json({
-                _id:user._id,
-                firstName:user.firstName,
-                lastName:user.lastName,
-                email:user.email
-            })
-        }
+//                 firstName,
+//                 lastName,
+//                 email,
+//                 password: hashPassword
+//             })
+//             //will be returning all fields except for the password
+//             res.redirect('/')
+//             // return res.json({
+//             //     _id:user._id,
+//             //     firstName:user.firstName,
+//             //     lastName:user.lastName,
+//             //     email:user.email
+//             // })
+//         }
 
-        return res.status(400).json({ message: 'Email already exists! do you want to register instead?' })
+//         return res.status(400).json({ message: 'Email already exists! do you want to register instead?' })
 
-    } catch (error) {
+//     } catch (error) {
 
        
-        throw Error(`Error while registering a new user : ${error}`)
-    }
-}
+//         throw Error(`Error while registering a new user : ${error}`)
+//     }
+// }
 
 
 const user_login= async(req, res)=>{
@@ -46,23 +115,19 @@ const user_login= async(req, res)=>{
     try {
     
         const {email, password}= req.body
-
+       // const{ password}=req.body.password
         if (!email || !password) {
 
-               //return res.render('home',{message:'Required fields missing'} )
-            // errors.push({msg:'Required fields missing'})
             req.flash('error_msg', 'Required fields are missing !')
             res.redirect('/')
-          
-            //return res.status(200).json({message:'Required filed missing'})
-           
+                  
         }
+      
         const user= await User.findOne({email})
-        if (!user){
-            //return res.status(200).json({message:'User not found. Do you want to register instead?'})
+        if (!user ){
+           
             req.flash('error_msg', 'User not found. Do you want to register instead?')
-            //return res.render('home', {message:'User not found. Do you want to register instead?'} )
-            res.redirect('/')
+             res.redirect('/')
         }
         if (user && await bcrypt.compare(password,user.password)) {
              const userResponse={
@@ -71,11 +136,12 @@ const user_login= async(req, res)=>{
                  firstName:user.firstName,
                  lastName:user.lastName
              }
-            
-             req.flash('success_msg', 'Welcome to your profile page')
+           
+             //req.flash('success_msg', 'Welcome to your profile page')
             res.render('profile', {userResponse})
           
-        } else{
+        } 
+        else{
             req.flash('error_msg','Email or Password does not match')
          
         }
@@ -83,12 +149,54 @@ const user_login= async(req, res)=>{
         throw Error(`Error while  Authenticating a User ${error}`)
     }
 }
+//delete user
+const delete_user=(req, res)=>{
+
+    const id= req.params.id;
+    User.findByIdAndDelete(id)
+        .then((result)=>{
+            req.flash('success_msg','You deleted your account successfuly')
+            res.redirect('/');
+           
+        })
+        .catch(err=>{
+            console.log(err);
+            
+        })
+
+}
+
+const profile_update_success=(req, res)=>{
+
+     res.render('success')
+}
+
+
+const update_user=async(req, res)=>{
+
+   // User.findOneAndUpdate({email})
+    
+        await User.findByIdAndUpdate({_id:req.params.id}, req.body,(err, docs)=>{
+
+        if (err) {
+            req.flash('error_msg', 'Could not fetch')
+        }
+        else{
+     
+           req.flash('success_msg', 'Profile update successfuly')
+            res.redirect('/update')
+        }
+
+})
+}
 
 
 
 
-// }
 module.exports = {
     register_user,
-    user_login
+    user_login,
+    update_user,
+    profile_update_success,
+    delete_user
 }
--- app/routes/Routes.js
+++ app/routes/Routes.js
@@ -1,15 +1,23 @@
 const express=require('express')
 const router = express.Router()
-const {register_user, user_login}=require('../controllers/userController')
+const {register_user, user_login,update_user,profile_update_success,delete_user
+}=require('../controllers/userController')
 
 
 router.get('/', (req,res)=>{
 
     res.render('home')
 })
+router.get('/register', (req, res)=>{
+
+    res.render('register');
+});
 
 
-router.post('/register', register_user)
+router.get('/delete/:id', delete_user)
+router.get('/update', profile_update_success)
 router.post('/login', user_login)
+router.post('/register', register_user)
+router.post('/update/:id', update_user)
 
 module.exports = router
